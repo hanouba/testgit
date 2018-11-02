6d42f3374aaa4b336c9a9ad0d395f2f47632ccbd
HanSir <han_belief@163.com>
2018-11-02 13:37:00 +0800

    app ionc set      and  splash ui  and  text fonts add

--- app/src/main/java/com/hansen/www/kot/ui/activity/SplashActivity.kt
+++ app/src/main/java/com/hansen/www/kot/ui/activity/SplashActivity.kt
@@ -10,16 +10,16 @@ import pub.devrel.easypermissions.EasyPermissions
 /**
  * 创建者 by ${HanSir} on 2018/11/2.
  * 版权所有  WELLTRANS.
- * 说明
+ * 说明      欢迎界面
  */
 class SplashActivity : BaseActivity() {
+
     override fun layoutId(): Int = R.layout.activity_splash
     override fun initData() {
-
     }
 
     override fun initView() {
-        bt_next.setOnClickListener { toMainActivity() }
+
         checkPermissions()
     }
 
@@ -31,6 +31,7 @@ class SplashActivity : BaseActivity() {
     private fun toMainActivity() {
         val intent = Intent(this,MainActivity::class.java)
         startActivity(intent)
+        finish()
     }
 
     override fun start() {
